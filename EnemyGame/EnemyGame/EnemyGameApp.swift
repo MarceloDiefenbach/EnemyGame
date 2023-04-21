d800e0c5e0181db7e9dc0b9fa3cedaceafd54f43
Lucas Dimer Justo <lucas.dimer.justo@hotmail.com>
2023-04-21 16:20:51 -0300

    projeto de vdd sem framework da idade media

---
+++ EnemyGame/EnemyGame/EnemyGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnemyGameApp.swift
+//  EnemyGame
+//
+//  Created by Lucas Justo on 21/04/23.
+//
+
+import SwiftUI
+
+@main
+struct EnemyGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
